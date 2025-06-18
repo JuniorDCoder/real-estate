bc4b40644c0288df988cb10f85a36272f09f6565
Junior DCoder <foryoungjuniorngu@gmail.com>
2025-06-18 10:11:16 +0100

    fix: Update AdminUserSeeder name

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             PropertySeeder::class,
-            AdminSeeder::class
+            AdminUserSeeder::class
         ]);
     }
 }
